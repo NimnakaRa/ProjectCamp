5a958422e2380bab45b0e4240cec6e19fd0a6501
Nimnaka <nimnaka.rajapaksha03@gmail.com>
2026-06-23 07:03:13 -0400

    design and express functionality

--- PracCamp _1/seeds/index.js
+++ PracCamp _1/seeds/index.js
@@ -20,10 +20,14 @@ const seedDB = async () => {
 
     for (let i = 1; i < 51; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            count: i
+            count: i,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis provident reprehenderit at molestiae eveniet, assumenda possimus pariatur ad sunt architecto, quaerat atque fuga necessitatibus asperiores! Hic cum veniam quam eligendi.',
+            price
 
 
         })
