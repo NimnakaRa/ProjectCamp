8dc7bedfcdf7c986833c853f4617ee25a20cf76a
Nimnaka <nimnaka.rajapaksha03@gmail.com>
2026-07-03 17:40:35 -0400

    restructing and reviews

--- PracCamp _1/models/campground.js
+++ PracCamp _1/models/campground.js
@@ -7,8 +7,24 @@ const CampgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
-    count: Number
+    count: Number,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 
 });
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
